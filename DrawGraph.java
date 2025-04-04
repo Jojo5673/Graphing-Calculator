fc648b998bbd4f87dc87a747d802869d6f2fa2be
Jojo5673 <judoudo2005@gmail.com>
2025-04-03 20:04:08 -0500

    System for Managing Points

--- DrawGraph.java
+++ DrawGraph.java
@@ -7,20 +7,25 @@ import org.scilab.forge.jlatexmath.TeXIcon;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 public class DrawGraph {
     public static void main(String[] args) {
         SimpleRegression regression = new SimpleRegression();
+        ArrayList<Point2D> data = new ArrayList<>();
+        data.add(new Point2D.Double(1, 2));
+        data.add(new Point2D.Double(2, 3));
+        data.add(new Point2D.Double(3, 5));
+        data.add(new Point2D.Double(4, 7));
+        data.add(new Point2D.Double(5, 11));
 
         // Add data points (x, y)
-        regression.addData(1, 2);
-        regression.addData(2, 3);
-        regression.addData(3, 5);
-        regression.addData(4, 7);
-        regression.addData(5, 11);
+        for (Point2D point : data) {
+            regression.addData(point.getX(), point.getY());
+        }
 
         // Get the slope (m) and intercept (b) for y = mx + b
         double slope = regression.getSlope();
@@ -29,20 +34,25 @@ public class DrawGraph {
         System.out.println("Best-fit line: y = " + slope + "x + " + intercept);
 
         // Generate data for the fitted line
-        List<Double> xData = new ArrayList<>();
-        List<Double> yData = new ArrayList<>();
+        List<Double> xFit = new ArrayList<>();
         List<Double> yFit = new ArrayList<>();
 
         for (double x = 0; x <= 6; x += 0.1) {
             double y = slope * x + intercept;
-            xData.add(x);
+            xFit.add(x);
             yFit.add(y);
         }
 
         // Plot using XChart
+        double[] x_data = new double[data.size()];
+        double[] y_data = new double[data.size()];
+        for (int i = 0; i < data.size(); i++) {
+            x_data[i] = data.get(i).getX();
+            y_data[i] = data.get(i).getY();
+        }
         XYChart chart = new XYChartBuilder().width(800).height(600).title("Linear Regression").xAxisTitle("X").yAxisTitle("Y").build();
-        chart.addSeries("Data Points", new double[]{1, 2, 3, 4, 5}, new double[]{2, 3, 5, 7, 11}).setMarker(SeriesMarkers.CIRCLE).setLineStyle(SeriesLines.NONE);
-        chart.addSeries("Regression Line", xData, yFit).setMarker(SeriesMarkers.NONE).setLineStyle(SeriesLines.SOLID);
+        chart.addSeries("Data Points", x_data, y_data).setMarker(SeriesMarkers.CIRCLE).setLineStyle(SeriesLines.NONE);
+        chart.addSeries("Regression Line", xFit, yFit).setMarker(SeriesMarkers.NONE).setLineStyle(SeriesLines.SOLID);
 
         new SwingWrapper<>(chart).displayChart();
     }
