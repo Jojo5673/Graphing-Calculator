d082adaa7b6ecc3d2d89e40d2326700f3fdab0d2
Jojo5673 <judoudo2005@gmail.com>
2025-04-08 20:01:30 -0500

    Comments!!!

--- Graph.java
+++ Graph.java
@@ -4,18 +4,22 @@ import java.time.Instant;
 
 import RegressionModels.*;
 
+//object class for a Graph that stores information for it and useful functions
 public class Graph {
-    private String id;
+    private String id; //unique hash generated to help with searching
     private String title;
     private ArrayList<Point2D.Double> points;
-    private transient RegressionModel regression = null;   //marked transient to avoid showing
+    private transient RegressionModel regression = null;   //marked transient to avoid showing up in json file (unable to store regression models)
     private String modelName = "";
-    private Instant timeStamp;
-    private Boolean connect_points = false;
-    private String imagePath = "";
+    private Instant timeStamp; //stores the instant that the graph was created to help with sorting
+    private Boolean connect_points = false; //used to determine whether to connect the points when rendering the graph
+    private String imagePath = ""; //stores the path of its exported image for use in the graph inventory manager display
 
-    public Graph() {}
+    public Graph() {} //no arguments constructor for serialization
+    //i.e. to store it in json, this is necessary so that the graph class can be built as the json storing functions cant send it arguments
+    //they instead use its getters and setters to get and load data
 
+   //getters
     public Boolean isConnect_points() {return connect_points;}
     public RegressionModel getRegression() {return regression;}
     public ArrayList<Point2D.Double> getPoints() {return points;}
@@ -24,6 +28,7 @@ public class Graph {
     public Instant getTimeStamp() {return timeStamp;}
     public String getImagePath() {return imagePath;}
 
+    //setters
     public void setImagePath(String imagePath) {this.imagePath = imagePath;}
     public void setConnect_points(Boolean connect_points) {this.connect_points = connect_points;}
     public void setPoints(ArrayList<Point2D.Double> points) {this.points = points;}
@@ -33,6 +38,8 @@ public class Graph {
         this.modelName = regression.getModelName();
     }
 
+    //constructor we use
+    //its gets the title and points, generates its ID, and stores when the graph was created
     public Graph(String title, ArrayList<Point2D.Double> points) {
         this.title = title;
         this.points = points;
@@ -40,10 +47,16 @@ public class Graph {
         this.timeStamp = Instant.now();
     }
 
+    //used to generate a unique code for each graph based on their title and when they were created
+    //format: graph-title-345698708263
     private void GenerateID(){
+        //removes all special characters from the string and replaces whitespaces with "-"
         String base = title.toLowerCase().replaceAll("\\s+", "-").replaceAll("[^a-z0-9\\-]", "");
+        //creates the appends the time to the title to create the id
         id = base + "-" + Instant.now().toEpochMilli();
     }
+
+    //used for when the graph is being loaded from storage since we cant store the regression classes in the file
     public void LoadRegression() {
         switch (modelName) {
             case "Sinusoidal":
--- GraphManager.java
+++ GraphManager.java
@@ -15,9 +15,20 @@ import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 public class GraphManager {
-    private static String JsonfilePath = "files/graphs.json";
+    public static String JsonfilePath = "files/graphs.json";
+    //keeps the data file's path for access anywhere
 
     public static void main(String[] args) {
+    //everything here right now is just for testing
+    //its likely this class won't even be the class with the main function but below is just a demonstration of how things should work
+    //the entire purpose of this class right now is to hold readGraphs and WriteGraphs methods
+    // however, it is open to manage anything with the graphs later in the future
+    //more than likely this class will be used to create graphs and deal with all their data though
+
+        //TO BE IMPLEMENTED: getting graph data, regression, and connect points flag from the ui
+
+        //DEMONSTRATION OF LOADING DATA INTO THE GRAPHS.
+        //we need to get this data from the user interface later
         ArrayList<Point2D.Double> data = new ArrayList<>();
         data.add(new Point2D.Double(1, 5));
         data.add(new Point2D.Double(2, 3));
@@ -32,15 +43,20 @@ public class GraphManager {
         data2.add(new Point2D.Double(6.5, 7));
         data2.add(new Point2D.Double(7, 11));
 
+        //DEMONSTRATION OF CONSTRUCTING GRAPHS AND SETTING THEIR PROPERTIES
+        // Their data needs to be initialised and packed into an arraylist of points
+        // The must be constructed with a title and said data
+        // the setREgression and setConnect_points will be utilised in the Graph Screen when the user is editing the graphs
         Graph graph = new Graph("Test Graph", data);
         Graph graph2 = new Graph("Test Graph2", data2);
         graph2.setRegression(new PolynomialRegression(data2, 2));
         graph.setConnect_points(true);
         graph.setRegression(new SinusoidalRegression(data));
 
-        //TO BE IMPLEMENTED: getting data and model from the ui
 
-        //saving graphs to the file
+        //DEMONSTRATION OF FILE READ/WRITE WITH GRAPHS
+        //they are written by passing an arraylist of any amount of graphs to writeGraphs()
+        //they are read in arraylists as well
         ArrayList<Graph> graphs = new ArrayList<>();
         graphs.add(graph);
         graphs.add(graph2);
@@ -51,29 +67,54 @@ public class GraphManager {
         }catch (IOException e){System.out.println("Unable to write graphs");}
 
         //reading graphs from the file
+        //read graphs returns an arraylist of graphs so they need to be accessed with .get() or with a for loop
         try{
-            for (Graph g : readGraphs()) {
-                g.LoadRegression();
-                GraphScreen.plot(g);
-            }
+            for (Graph g : readGraphs())
+                GraphScreen.plot(g); //this is the function to display graphs by the way
         }catch (IOException e){System.out.println("Unable to read to file");}
     }
 
+    //it's important to clarify why all this has been done to store the file in a json
+    //json stores key value pairs kind of like a hashmap or dictionary
+    //This means that our file data is going to be extremely human-readable which is good for debuggin
+    //Also using the gson library allows us to outsource all the hard work of parsing our custom Graph object when reading its data from a file
+    //a text file means we'd have to read straight text and implement the logic to handle all of our data (nobody wants to do this)
+    //other file methods are lowk too much work to learn so here we are
+
+    //function that handles loading the graphs stored in the file
     public static ArrayList<Graph> readGraphs() throws IOException {
+        //json handling is done by the Gson library (one of the external packages we imported)
+        //we first create a json manager (an instance of Gson) and register the type adapter for our Instant class
+        //the type adapter tells the json manager how to handle the Instant class (stores our graph timestamp)
+        //setPrettyPrinting means everything wont go in one line and it will get formatted nice and pretty
         Gson gson = new GsonBuilder().registerTypeAdapter(Instant.class, new InstantAdapter()).setPrettyPrinting().create();
+        //we read the data from the file into a string and gson parses this string and loads our data into a list of graphs
         String json = Files.readString(Paths.get(JsonfilePath));
-        Type listType = new TypeToken<ArrayList<Graph>>(){}.getType();
-        return gson.fromJson(json, listType);
+        Type listType = new TypeToken<ArrayList<Graph>>(){}.getType(); //this tells gson what type of object we are lokoing for (Graph)
+        ArrayList<Graph>graphs = gson.fromJson(json, listType); //we use the type we defined and the json string to load everything
+        for (Graph g : graphs) {
+            g.LoadRegression(); //this sets the regression based on the regression model's name stored for the graph.
+            //we are unable to store regression models. they are a bit too complex and writing logic to make Gson parse it is too much work
+        }
+        return graphs;
     }
 
+    //function that handles storing the graphs in the file
     public static void writeGraphs(ArrayList<Graph> graphs) throws IOException {
+        //see readGraphs for explanations on the json stuff
         Gson gson = new GsonBuilder().registerTypeAdapter(Instant.class, new InstantAdapter()).setPrettyPrinting().create();
+
+        //loads a file writer linked to our json file
         FileWriter writer = new FileWriter(JsonfilePath);
+        //converts our data into a json string
         String json = gson.toJson(graphs);
+        //writes the json string to our file
         writer.write(json);
         writer.close();
     }
 
+    //this can be safely ignored, but they just tell the json file manager how to handle the Instant class (once again stores our timestamps for graphs)
+    //its not normally able to handle complex classes by default so it needs to be told how
     static class InstantAdapter implements JsonSerializer<Instant>, JsonDeserializer<Instant> {
         public JsonElement serialize(Instant src, Type typeOfSrc, JsonSerializationContext context) {
             return new JsonPrimitive(src.toString()); // ISO-8601 format
